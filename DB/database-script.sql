518589a44fa3bdd5e72e5c92fceebfd8a0768eff
VitorMendesco <vitor.mcosta@sptech.school>
2022-09-27 01:10:06 -0300

    refactor: db ERD and script updated

--- DB/database-script.sql
+++ DB/database-script.sql
@@ -10,7 +10,8 @@ USE MANOSecurity;
 CREATE TABLE company (
 	idCompany INT PRIMARY KEY AUTO_INCREMENT,
 	companyName VARCHAR (50),
-	companyEmail VARCHAR (90)
+	companyEmail VARCHAR (90),
+    CNPJ VARCHAR (14)
 );
 
 /*Description of the company table:*/
@@ -18,11 +19,11 @@ DESC company;
 
 /*Company Data insertion:*/
 INSERT INTO company VALUES
-    (null, "Colegio da PM Penha", "colegiopmpen@outlook.com"),
-    (null, "Colegio Dom Bosco", "colegiodombosco@outlook.com"),
-    (null, "Escola Adventista do Sul", "adventistadosul@outlook.com"),
-    (null, "Colegio da PM Centro", "colegiopmcen@outlook.com"),
-    (null, "Colegio Grili Magalhães", "grilimagalhaes@outlook.com");
+    (null, "Colegio da PM Penha", "colegiopmpen@outlook.com", "12345678901234"),
+    (null, "Colegio Dom Bosco", "colegiodombosco@outlook.com", "09876543210987"),
+    (null, "Escola Adventista do Sul", "adventistadosul@outlook.com", "12345654321234"),
+    (null, "Colegio da PM Centro", "colegiopmcen@outlook.com", "09876567890987"),
+    (null, "Colegio Grili Magalhães", "grilimagalhaes@outlook.com", "10293847565748");
 
 /*Creation of Consumer table*/
 CREATE TABLE consumer (
@@ -31,9 +32,12 @@ CREATE TABLE consumer (
 	consumerEmail VARCHAR (50),
 	consumerPassword VARBINARY(150),
 	management VARCHAR (8), CONSTRAINT chkManagement CHECK 
-	(management = "MASTER" or management = "ADMIN" or management = "ANALIST"),
-	fkCompany INT,
-	FOREIGN KEY (fkCompany) REFERENCES Company(idCompany)
+	(management = "MASTER" or management = "ADMIN" or management = "ANALYST"),	
+    dtAdded DATETIME DEFAULT CURRENT_TIMESTAMP,
+	fkManager INT,
+	FOREIGN KEY (fkManager) REFERENCES consumer(idConsumer),
+    fkCompany INT,
+	FOREIGN KEY (fkCompany) REFERENCES company(idCompany)
 );
 
 /*Description of Consumer table*/
@@ -41,12 +45,13 @@ DESC Consumer;
 
 /*Consumer data insertion*/
 INSERT INTO consumer VALUES 
-    (null, "Vinicius Mengo", "vinicinho@gmail.com", "Ab45579021", "ADMIN", 2),
-    (null, "Danillo Borba", "danborba@gmail.com", "Baa34569034", "MASTER", 1),
-    (null, "Paulo Ranea", "paulogono@hotmail.com", "UUU38535850", "ADMIN", 3),
-    (null, "Andrey Gigabyte", "andgiga@gmail.com", "Dew255948947", "ADMIN", 4),
-    (null, "Arthur Itaquerense", "artitaq@gmail.com", "bwf36234366", "ADMIN", 5),
-    (null, "Alvarenga Pizza", "alvspizza@outlook.com", "Bafw39597927", "ANALIST", 3);
+	(null, "test", "test@gmail.com", AES_ENCRYPT("1234", "corinthians"), "MASTER", default, null, 1),
+    (null, "Vinicius Mengo", "vinicinho@gmail.com", AES_ENCRYPT("Ab45579021", "corinthians"), "ADMIN", default, null, 2),
+    (null, "Danillo Borba", "danborba@gmail.com", AES_ENCRYPT("Baa34569034", "corinthians"), "MASTER", default, 1, 1),
+    (null, "Paulo Ranea", "paulogono@hotmail.com", AES_ENCRYPT("UUU38535850", "corinthians"), "ADMIN", default, 2, 2),
+    (null, "Andrey Gigabyte", "andgiga@gmail.com", AES_ENCRYPT("Dew255948947", "corinthians"), "ADMIN", default, 3, 1),
+    (null, "Arthur Itaquerense", "artitaq@gmail.com", AES_ENCRYPT("bwf36234366", "corinthians"), "ANALYST", default, 1, 1),
+    (null, "Alvarenga Pizza", "alvspizza@outlook.com", AES_ENCRYPT("Bafw39597927", "corinthians"), "ANALYST", default, null, 3);
 
 /*Creation of Family table*/
 CREATE TABLE family (
@@ -69,6 +74,58 @@ INSERT INTO family VALUES
     (null, "Group 8", "Junior", 4),
     (null, "21° Setor", "Student", 1);
 
+/*Creation of Machine table*/
+CREATE TABLE machine (
+	idMachine INT PRIMARY KEY AUTO_INCREMENT,
+    machineName VARCHAR (20),
+    dtAdded DATETIME DEFAULT CURRENT_TIMESTAMP,
+    isUsing CHAR(3) DEFAULT 'not', CONSTRAINT chkMachineUse CHECK 
+	(isUsing = "yes" OR isUsing = "not"),
+    fkConsumerAdder INT,
+    FOREIGN KEY (fkConsumerAdder) REFERENCES consumer(idConsumer),
+	fkCompany INT,
+	FOREIGN KEY (fkCompany) REFERENCES company(idCompany),
+	fkFamily INT,
+    FOREIGN KEY (fkFamily) REFERENCES family(idFamily)
+);
+
+/*Description of Machine table*/
+DESC machine;
+
+/*Machine data insertion*/
+INSERT INTO machine VALUES
+	(null, "Chrome01", NOW(), DEFAULT, 1, 2, 2),
+    (null, "Chrome02", NOW(), DEFAULT, 2, 2, 3),
+    (null, "Chrome03", NOW(), DEFAULT, 2, 2, 4),
+    (null, "Chrome04", NOW(), DEFAULT, 3, 2, 5),
+    (null, "Dell01", NOW(), DEFAULT, 4, 1, 3),
+    (null, "Samsung03", NOW(), DEFAULT, 1, 5, 2),
+    (null, "Samsung06", NOW(), DEFAULT, 2, 5, 3);
+
+/*Creation of Hardware table*/
+CREATE TABLE hardware (
+	idHardware INT PRIMARY KEY AUTO_INCREMENT,
+	CPU_Util TINYINT,
+	RAM_Util TINYINT,
+	HardDisk_Util TINYINT,
+	Memory_Util TINYINT,
+	CPU_Temp INT,
+	fkMachine INT,
+	FOREIGN KEY (fkMachine) REFERENCES machine(idMachine)
+);
+
+/*Description of Hardware table*/
+DESC hardware;
+
+/*Hardware data insertion*/
+INSERT INTO hardware VALUES 
+	(null, 15, 42, 5, 10, 10, 2),
+	(null, 34, 5, 25, 44, 11, 5),
+	(null, 46, 26, 41, 52, 9, 1),
+	(null, 56, 35, 47, 87, 13, 3),
+	(null, 44, 54, 74, 63, 8, 5),
+	(null, 17, 51, 73, 43, 11, 4);
+    
 /*Creation of Operation table*/
 CREATE TABLE operation (
 	idOperation INT PRIMARY KEY AUTO_INCREMENT,
@@ -88,105 +145,29 @@ INSERT INTO operation VALUES
     (null, "Visual Studio Code", "https://code.visualstudio.com/", "desktop"),
     (null, "MySQL Workbench", "https://www.mysql.com/", "desktop"),
     (null, "Outlook", "https://outlook.live.com/owa/", "web"),
-    (null, "Youtube", "https://www.youtube.com/?gl=BR&hl=pt", "web");
+    (null, "Youtube", "https://www.youtube.com/?gl=BR&hl=pt", "web");    
 
-/*Creation of Machine table*/
-CREATE TABLE machine (
-	idMachine INT PRIMARY KEY AUTO_INCREMENT,
-    machineName VARCHAR (20),
-    dtAdded DATETIME DEFAULT CURRENT_TIMESTAMP,
-	nameUserAdder VARCHAR (50),
-    fkConsumerAdder INT,
-    FOREIGN KEY (fkConsumerAdder) REFERENCES consumer(idConsumer),
+/*Creation of CompanyOperations table*/
+CREATE TABLE companyOperations (
+	idCompanyOperations INT AUTO_INCREMENT,
 	fkCompany INT,
 	FOREIGN KEY (fkCompany) REFERENCES company(idCompany),
-	fkFamily INT,
-    FOREIGN KEY (fkFamily) REFERENCES family(idFamily)
-);
-
-/*Description of Machine table*/
-DESC machine;
-
-/*Machine data insertion*/
-INSERT INTO machine VALUES
-	(null, "Chrome01", NOW(), "Marcos", 1, 2, 2),
-    (null, "Chrome02", NOW(), "Rogério", 2, 2, 3),
-    (null, "Chrome03", NOW(), "Rogério", 2, 2, 4),
-    (null, "Chrome04", NOW(), "Caio", 3, 2, 5),
-    (null, "Dell01", NOW(), "Claudio", 4, 1, 3),
-    (null, "Samsung03", NOW(), "Miguel", 1, 5, 2),
-    (null, "Samsung06", NOW(), "Guilherme", 2, 5, 3)
-    ;
-
-CREATE TABLE hardware (
-	idHardware INT PRIMARY KEY AUTO_INCREMENT,
-	CPU_Util TINYINT,
-	RAM_Util TINYINT,
-	HardDisk_Util TINYINT,
-	Memory_Util TINYINT,
-	CPU_Temp INT,
-	fkMachine INT,
-	FOREIGN KEY (fkMachine) REFERENCES machine(idMachine),
 	fkOperation INT,
-	FOREIGN KEY (fkOperation) REFERENCES operation(idOperation)	
+    FOREIGN KEY (fkOperation) REFERENCES operation(idOperation),
+    PRIMARY KEY (idCompanyOperations, fkCompany, fkOperation)
 );
-
-DESC hardware;
-
-INSERT INTO hardware VALUES 
-	(null, 20, 15, 42, 5, 10, 10, 2),
-	(null, 17, 34, 5, 25, 44, 11, 5),
-	(null, 25, 46, 26, 41, 52, 9, 1),
-	(null, 5, 56, 35, 47, 87, 13, 3),
-	(null, 21, 44, 54, 74, 63, 8, 5),
-	(null, 12, 17, 51, 73, 43, 11, 4);
     
-/*Creation of LogKilledOperation table*/
-CREATE TABLE logKilledOperation (
-	idLogKilledOperation INT AUTO_INCREMENT,
-	dtOperationKill DATETIME DEFAULT CURRENT_TIMESTAMP, 
-	operationName VARCHAR (50),
-	fkMachine INT,
-    FOREIGN KEY (fkMachine) REFERENCES machine(idMachine),
-    PRIMARY KEY (idLogKilledOperation, fkMachine)
-);
+/*Description of CompanyOperations table*/    
+DESC companyOperations;
 
-/*Description of LogKilledOperation table*/
-DESC logKilledOperation;
-
-/*LogKilledOperation data insertion*/
-INSERT INTO logKilledOperation VALUES 
-	(null, NOW(), "vscode.running", 10),
-    (null, NOW(), "steam.com.running", 9),
-    (null, NOW(), "epicgames.com.running", 13),
-    (null, NOW(), "minecraft.com.running", 8),
-    (null, NOW(), "steam.com.running", 11), 
-    (null, NOW(), "friv.com.running", 9);
-
-/*Creation of OperationRunning table*/
-CREATE TABLE operationRunning (
-	idOperationRunning INT AUTO_INCREMENT,
-	fkMachine INT,
-	FOREIGN KEY (fkMachine) REFERENCES Machine(idMachine),
-    fkOperation INT,
-    FOREIGN KEY (fkOperation) REFERENCES Operation(idOperation),
-    PRIMARY KEY (idOperationRunning, fkMachine, fkOperation),
-    operationStats CHAR (7), CONSTRAINT chkOperationStats CHECK 
-	(operationStats = "running" or operationStats = "stopped"),
-    lastUsed DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-/*Description of OperationRunning table*/
-DESC operationRunning;
-
-/*OperationRunning data insertion*/
-INSERT INTO operationRunning VALUES
-	(null, 10, 4, "running", NOW()),
-    (null, 13, 3, "stopped", NOW()),
-    (null, 9, 2, "running", NOW()),
-    (null, 8, 4, "running", NOW()),
-    (null, 11, 5, "stopped", NOW()),
-    (null, 12, 2, "running", NOW());
+/*CompanyOperations data insertion*/
+INSERT INTO companyOperations VALUES 
+	(null, 4, 3),
+    (null, 1, 3),
+    (null, 5, 4),
+    (null, 3, 1),
+    (null, 2, 2),
+    (null, 2, 5);
 
 /*Creation of OperationLog table*/
 CREATE TABLE operationLog (
@@ -210,28 +191,52 @@ INSERT INTO operationLog VALUES
     (null, 5, 1),
     (null, 4, 4);
     
-/*Creation of CompanyOperations table*/
-CREATE TABLE companyOperations (
-	idCompanyOperations INT AUTO_INCREMENT,
-	fkCompany INT,
-	FOREIGN KEY (fkCompany) REFERENCES company(idCompany),
-	fkOperation INT,
-    FOREIGN KEY (fkOperation) REFERENCES operation(idOperation),
-    PRIMARY KEY (idCompanyOperations, fkCompany, fkOperation)
+/*Creation of OperationRunning table*/
+CREATE TABLE operationRunning (
+	idOperationRunning INT AUTO_INCREMENT,
+	fkMachine INT,
+	FOREIGN KEY (fkMachine) REFERENCES Machine(idMachine),
+    fkOperation INT,
+    FOREIGN KEY (fkOperation) REFERENCES Operation(idOperation),
+    PRIMARY KEY (idOperationRunning, fkMachine, fkOperation),
+    operationStats CHAR (7), CONSTRAINT chkOperationStats CHECK 
+	(operationStats = "running" or operationStats = "stopped"),
+    lastUsed DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+/*Description of OperationRunning table*/
+DESC operationRunning;
+
+/*OperationRunning data insertion*/
+INSERT INTO operationRunning VALUES
+	(null, 1, 4, "running", NOW()),
+    (null, 2, 3, "stopped", NOW()),
+    (null, 3, 2, "running", NOW()),
+    (null, 4, 4, "running", NOW()),
+    (null, 5, 5, "stopped", NOW()),
+    (null, 6, 2, "running", NOW());
     
-/*Description of CompanyOperations table*/    
-DESC companyOperations;
+/*Creation of LogKilledOperation table*/
+CREATE TABLE logKilledOperation (
+	idLogKilledOperation INT AUTO_INCREMENT,
+	dtOperationKill DATETIME DEFAULT CURRENT_TIMESTAMP, 
+	operationName VARCHAR (50),
+	fkMachine INT,
+    FOREIGN KEY (fkMachine) REFERENCES machine(idMachine),
+    PRIMARY KEY (idLogKilledOperation, fkMachine)
+);
 
-/*CompanyOperations data insertion*/
-INSERT INTO companyOperations VALUES 
-	(null, 4, 3),
-    (null, 1, 3),
-    (null, 5, 4),
-    (null, 3, 1),
-    (null, 2, 2),
-    (null, 2, 5);
+/*Description of LogKilledOperation table*/
+DESC logKilledOperation;
 
+/*LogKilledOperation data insertion*/
+INSERT INTO logKilledOperation VALUES 
+	(null, NOW(), "vscode.running", 2),
+    (null, NOW(), "steam.com.running", 3),
+    (null, NOW(), "epicgames.com.running", 4),
+    (null, NOW(), "minecraft.com.running", 5),
+    (null, NOW(), "steam.com.running", 6), 
+    (null, NOW(), "friv.com.running", 7);
 
 /*Beginning of the selects (data showing):*/
 
@@ -245,6 +250,11 @@ SELECT * FROM family;
 SELECT * FROM operationLog;
 SELECT * FROM logKilledOperation;
 SELECT * FROM companyOperations;
+
+/*-----------------------------------------------------*/
+
+
+
 
 /*-----------------------------------------------------*/
 
